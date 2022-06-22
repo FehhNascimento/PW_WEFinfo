41aa0da58ebc4dc16f770e7dbe4902fb225d3895
KidFlashReverse <wallacyj.que.j@gmail.com>
2022-06-21 23:02:59 -0300

    Login funfando

--- php/pesquisar.php
+++ php/pesquisar.php
@@ -1,23 +1,25 @@
 <?php  
 	$pdo = require 'conexao.php';
 	$sql = 'select * from paobanco';
+	$n = 0;
 
 	foreach ($pdo->query($sql) as $key => $valor) {
-		
-		if (($_POST['email']==$valor['Email']) && ($_POST['senha']==$valor['Senha'])) {
-?>
+		if (($_POST['email']==$valor['Email']) && ($_POST['senha']==$valor['Senha']) && ($valor['ADM'] == 1)){
+			echo "
 			<script>
-				alert("Login Realizado");
-				window.location='../index.php';
+				alert('ADM Logado');
+				window.location = '../index.php';
 			</script>
-<?php		
+		";
+		} elseif (($_POST['email']==$valor['Email']) && ($_POST['senha']==$valor['Senha'])) {
+			echo "
+				<script>
+					alert('Login Realizado');
+					window.location = '../index.php';
+				</script>
+			";
 		}else{
-?>
-			<script>
-				alert("Credenciais Incorretas!\nFaça o Cadastro ou Tente de novo");
-				window,location='../pages/login.php';
-			</script>
-<?php
+			echo "Faça Cadastro";
 		}
 	}
 ?>
